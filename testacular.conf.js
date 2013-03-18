00061f39d4c6920473623edea4073145bb39a032
voice <VoiceTime@yandex.ru>
2013-03-18 04:16:30 +0400

    1

--- testacular.conf.js
+++ testacular.conf.js
@@ -13,11 +13,6 @@ basePath = '';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'test/client/mocks.js',
-  'static/testacular.src.js',
-  'test/client/mocks/ObjectModel.js',
-  'adapter/*.src.js',
-  'test/client/*.spec.js',
   'kperf.js',
    'kperfSpec.js'
 ];
