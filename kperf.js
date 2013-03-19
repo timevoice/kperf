b2a143e064dff84027e3e715880975652bebf910
voice <VoiceTime@yandex.ru>
2013-03-20 00:10:50 +0400

    1

--- kperf.js
+++ kperf.js
@@ -5,20 +5,3 @@
  * Time: 22:47
  * To change this template use File | Settings | File Templates.
  */
-function returnOne() {
-    return 1;
-
-}
-
-function walk() {
-    a = 2;
-    b = 3;
-    return a + b;
-}
-
-function run(){
-    return 2 +3;
-}
-
-var move = kperf( walk, run);
-move();
--- kperfSpec.js
+++ kperfSpec.js
@@ -5,9 +5,34 @@
  * Time: 22:49
  * To change this template use File | Settings |  File Templates.
  */
+
+
+function returnOne() {
+    return 1;
+
+}
+
+
 describe("returnOne", function() {
     it("WoW", function() {
         expect(returnOne(0)).toBe(1);
     });
 
 });
+
+
+
+
+walk =  function () {
+    a = 2;
+    b = 3;
+    return a + b;
+}
+
+run = function (){
+    return 2 + 3;
+}
+
+var supers = kperf( walk, run);
+
+expect( supers() ).toBe(5)  
